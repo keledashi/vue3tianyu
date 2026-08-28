84fd343bdc884cec0e055c031fc7230ebda2350f
jinitaimei <5236029+keledashi@user.noreply.gitee.com>
2026-08-28 18:34:07 +0800

    update

---
+++ 03组件化/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+    <span class="hello">HELLO</span>    
+</template>
+
+<script>
+    export default {
+        name : "Hello"
+    }
+</script>
+
+<style>
+    .hello{
+        color:blueviolet;
+        font-weight: bold;
+        background-color: cyan;
+    }
+</style>
